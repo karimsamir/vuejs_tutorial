e7b6978bf66d2eda9ff0824ea8c619f9998a2b90
Karim Samir <karim5977@gmail.com>
2025-12-07 16:50:31 +0200

    247. Registering as a Coach: The Form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,6 +1,20 @@
 <template>
 	<section>
-		<h1>Register as a Coach</h1>
-		<p>Registration form goes here.</p>
+		<base-card>
+			<h2>Register as a Coach</h2>
+			<p>Registration form goes here.</p>
+			<coach-form></coach-form>
+		</base-card>
 	</section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+	components: {
+		CoachForm,
+	},
+	
+}
+</script>
