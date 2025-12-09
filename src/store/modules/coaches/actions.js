2fb571cce9dd4f8fe1f97764906158a36468775d
Karim Samir <karim5977@gmail.com>
2025-12-09 02:13:06 +0200

    254. Sending a PUT Http Request to Store Coach Data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,42 @@
 export default {
-    registerCoach(context, data) {
-        const coachData = {
-        //   id: new Date().toISOString(),
-          id: context.rootGetters.userId,
-          firstName: data.first,
-          lastName: data.last,
-          description: data.desc,
-          hourlyRate: data.rate,
-          areas: data.areas,
-        };
-        context.commit('registerCoach', coachData);
-    },
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+
+    const coachData = {
+      //   id: new Date().toISOString(),
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+
+    const response = await fetch(
+      // 'https://vue-http-demo-6f7c3-default-rtdb.firebaseio.com/coaches/' +
+      `https://vue-coach-83b2b-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+      if (response.ok) {
+        // const responseData = await response.json();
+        context.commit('registerCoach', { ...coachData, id: userId });
+      }
+
+
+    // .then((response) => {
+    //   if (!response.ok) {
+    //     // error ...
+    //   }
+    //   // const responseData = await response.json();
+    //   context.commit('registerCoach', coachData);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+    //
+    // context.commit('registerCoach', {...coachData, id: userId});
+  },
 };
