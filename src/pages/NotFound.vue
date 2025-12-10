3c991ebd5974501c8eb12f6bb44aae98f8a618cd
Karim Samir <karim5977@gmail.com>
2025-12-10 02:24:21 +0200

    261. The "Not Found" Page & Summary

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
-    Not Found!
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>Page not found, check <router-link to="/coaches">coaches</router-link> page....</p>
+        </base-card>
+    </section>
+
 </template>
