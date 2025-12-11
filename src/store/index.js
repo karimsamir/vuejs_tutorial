bf63f65a100f7d96e13f09310eb478e528001c45
Karim Samir <karim5977@gmail.com>
2025-12-11 19:15:59 +0200

    267. Preparing Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,19 @@
 import { createStore } from "vuex";
 import coachesModule from "./modules/coaches/index.js";
 import requestsModule from "./modules/requests/index.js";
-// import getters from "./modules/coaches/getters.js";
-// import requestsModule from "./modules/requests/index.js";
-// import authModule from "./modules/auth/index.js";
+import authModule from "./modules/auth/index.js";
 
 const store = createStore({
     namepaced: true,
     modules: {
         coaches: coachesModule,
         requests: requestsModule,
+        auth: authModule,
     },
-    state() {
-        return {
-            userId: "c3",
-        };
-    },
-    getters: {
-        userId(state) {
-            return state.userId;
-        },
-        
-    },
+    // getters: {
+
+
+    // },
 });
 
 export default store;
