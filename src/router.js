1ac21a62fcc442013033060aea342037066067fa
Karim Samir <karim5977@gmail.com>
2025-12-11 19:15:32 +0200

    266. Adding an Authentication Page (Login & Signup)

--- src/router.js
+++ src/router.js
@@ -6,7 +6,9 @@ import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestReceived from './pages/requests/RequestReceived.vue';
+
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const routes = [
   { path: '/', redirect: '/coaches' },
@@ -26,6 +28,7 @@ const routes = [
   },
   { path: '/register', component: CoachRegistration },
   { path: '/requests', component: RequestReceived },
+  { path: '/auth',component: UserAuth},
   {
     path: '/:notfound(.*)',
     component: NotFound,
