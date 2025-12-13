6d937ec50e2c188f606e872bffe6c6898e6e852d
Karim Samir <karim5977@gmail.com>
2025-12-13 23:53:36 +0200

    273. Adding a "Logout" Action & Flow

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -12,6 +12,9 @@
         <li v-else>
           <router-link to="/auth">Login</router-link>
         </li>
+        <li v-if="isLoggedIn">
+          <base-button @click="logout">Logout</base-button>
+        </li>
         <!-- <li><router-link to="/register">Register as Coach</router-link></li> -->
       </ul>
     </nav>
@@ -23,6 +26,11 @@ export default {
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
     },
   },
 };
